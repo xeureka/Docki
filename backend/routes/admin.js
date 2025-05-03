0c03adf0da30de749bb7c32bef7bae2892442e60
xeureka <teklemariameureka@gmail.com>
2025-05-03 12:29:06 +0000

    adding of book works fine now

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,92 +1,54 @@
 
+/** 
+    Post /admin/books
+    PUT /admin/books
+    Delete /admin/books
 
-require('dotenv').config()
+*/
+
 const express = require('express')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
+const multer = require('multer')
+const Books = require('../models/books.model')
 
-const Admins = require('../models/admin.model')
 const router = express.Router()
 
-// List of all the admins
-router.get('/', async (req,res) =>{
 
-    try {
-
-        const admins = await Admins.find()
-
-        res.json(admins)
-
-    } catch (error) {
-        console.error('Error Fetching admins, ',error.message)
-        return res.status(401)
-    }
+router.get('/', (req,res) => {
+    res.send('Admin Dashboard !!')
 })
 
-// registering new admin
+// route to add new books
+router.post('/books', async (req,res) => {
 
-router.post('/register', async (req,res) => {
+ 
 
     try {
 
-        let salt = await bcrypt.genSalt(10)
+        let book = await Books.findOne({title: req.body.title})
 
-        let newAdmin = await Admins.findOne({email: req.body.email})
-
-        if (newAdmin){
-            return res.json('User Already Regirstered !!')
-            // redirect to the sign up route
+        if (book){
+            return res.status(401).json('Book Already Added !!')
         }
 
-        newAdmin = new Admins({
-            name: req.body.name,
-            email: req.body.email,
-            password: await bcrypt.hash(req.body.password, salt)
+        book = new Books({
+            title: req.body.title,
+            author:req.body.author,
+            publicationDate: req.body.publicationDate,
+            bookFile: req.body.bookFile,
+            tags:req.body.tags
         })
 
-        let token = jwt.sign({id: newAdmin._id, email: newAdmin.email,},process.env.jwtPrivateKey)
+        await book.save()
 
-        await newAdmin.save()
+        res.json(book)
 
-        res.json(newAdmin)
         
     } catch (error) {
-        console.log('Error creating admin, ',error.message)
-        return res.status(401)
-    }
+
+        console.log(error)
+
+        return res.status(500).json({ error: error.message });    }
 })
 
 
-router.post('/login', async (req,res) => {
-
-    try {
-
-        let adminUser = await Admins.findOne({email: req.body.email})
-
-        if (!adminUser){
-            return res.status(403).json('No admin with a given email !!')
-        }
-
-        const password = req.body.password;
-
-        let validPassword = await bcrypt.compare(password, adminUser.password)
-
-        if (!validPassword){
-            return res.status(401).json('Invalid username or password !!')
-        }
-
-        let token = jwt.sign({_id: adminUser._id,email: adminUser.email},process.env.jwtPrivateKey)
-
-        return res.header('x-auth-token',token).json(token)
-
-        // redirect the user to the admin dashboard
-        
-    } catch (error) {
-        console.log('Error loggin user: ',error.message)
-    }
-
-})
-
-
-module.exports = router
-
+module.exports = router;
--- text.js
+++
@@ -1,6 +0,0 @@
-
-
-
-// Lets create book schema and route
-
-// for adding, storing and editing of books
