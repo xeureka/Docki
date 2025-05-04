0e3bbcfc736894fbfa0f5f0f3bf9dfddfe1ed8c4
Asher Samuel <ashersam116@gmail.com>
2025-05-04 21:07:55 +0300

    fix/user name shouldn't be required

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,113 +1,110 @@
+require("dotenv").config();
+const { doHash, doHashValidation } = require("../utils/hashing");
+const { signUpSchema } = require("../middleware/validator");
+const jwt = require("jsonwebtoken");
+const Users = require("../models/users.model");
 
+async function signUp(req, res) {
+  const { email, password } = req.body;
 
-require('dotenv').config()
-const {doHash,doHashValidation} = require('../utils/hashing')
-const {signUpSchema} = require('../middleware/validator') 
-const jwt = require('jsonwebtoken')
-const Users = require('../models/users.model')
+  try {
+    const { error, value } = signUpSchema.validate({ email, password });
 
-async function signUp(req,res) {
-
-    const {email,password} = req.body
-
-
-    try {
-
-        const {error,value} = signUpSchema.validate({email,password})
-
-        if (error){
-            return res.status(401).json({success: false, message: error.details[0].message})
-        }
-
-
-        const isExistingUser = await Users.findOne({email})
-
-        if (isExistingUser){
-            return res.status(401).json({success: false, message: 'User already exists'})
-        }
-
-        const hashedPassword = await doHash(password)
-
-        const newUser = new Users({
-            name,
-            email,
-            password: hashedPassword
-        })
-
-        const result = await newUser.save()
-
-        result.password = undefined
-
-        res.status(201).json({
-            success: true,
-            message: 'User created Sucessful !!',
-            result
-        })
-        
-    } catch (error) {
-        console.log(error)
+    if (error) {
+      return res
+        .status(401)
+        .json({ success: false, message: error.details[0].message });
     }
-    
+
+    const isExistingUser = await Users.findOne({ email });
+
+    if (isExistingUser) {
+      return res
+        .status(401)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await doHash(password);
+
+    const newUser = new Users({
+      email,
+      password: hashedPassword,
+    });
+
+    const result = await newUser.save();
+
+    result.password = undefined;
+
+    res.status(201).json({
+      success: true,
+      message: "User created Sucessful !!",
+      result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
+async function signIn(req, res) {
+  const { email, password } = req.body;
 
-async function signIn(req,res){
+  try {
+    const { error, value } = signUpSchema.validate({ email, password });
 
-    const {email,password} = req.body
+    if (error) {
+      return res
+        .status(401)
+        .json({ success: false, message: error.details[0].message });
+    }
 
-    try {
-        const {error,value} = signUpSchema.validate({email,password})
+    const existingUser = await Users.findOne({ email }).select("+password");
 
-        if (error){
-            return res.status(401).json({success: false, message: error.details[0].message})
-        }
+    if (!existingUser) {
+      return res
+        .status(401)
+        .json({ success: false, message: "User dont exists" });
+    }
 
-        const existingUser = await Users.findOne({email}).select('+password')
+    const result = await doHashValidation(password, existingUser.password);
 
-        if (!existingUser){
-            return res.status(401).json({success: false, message: 'User dont exists'})
-        }
-        
-        const result = await doHashValidation(password,existingUser.password)
+    if (!result) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid credentials !!" });
+    }
 
-        if (!result){
-            return res.status(401).json({success: false, message: 'Invalid credentials !!'})
-        }
+    const token = jwt.sign(
+      {
+        useId: existingUser._id,
+        email: existingUser.email,
+        verified: existingUser.verified,
+      },
+      process.env.jwtPrivateKey,
+      { expiresIn: "8h" }
+    );
 
-        const token = jwt.sign({
-            useId: existingUser._id,
-            email: existingUser.email,
-            verified: existingUser.verified
-        },
-        process.env.jwtPrivateKey,{expiresIn: '8h'}
-    
-    )
-
-    res.cookie('Authorization','Bearer '+ token,{expires: new Date(Date.now() + 8*3600000)})
+    res.cookie("Authorization", "Bearer " + token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
     res.json({
-        success: true,
-        token,
-        message: 'logged in successfully'
-    })
-
-            
-    } catch (error) {
-        console.log(error)
-    }
+      success: true,
+      token,
+      message: "logged in successfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-async function signOut(req,res){
-
-    res.clearCookie('Authorization').status(200).json({
-        success: true, message: 'Loged out successfully !!'
-    })
+async function signOut(req, res) {
+  res.clearCookie("Authorization").status(200).json({
+    success: true,
+    message: "Loged out successfully !!",
+  });
 }
-
-
 
 module.exports = {
-    signUp,
-    signIn,
-    signOut,
-}
-
+  signUp,
+  signIn,
+  signOut,
+};
