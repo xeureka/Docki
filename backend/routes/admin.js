184c762b3e69739e40580417aa94abe51caa6215
Asher Samuel <ashersam116@gmail.com>
2025-05-04 19:40:41 +0300

    that wasn't necessary

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,92 +1,74 @@
+const express = require("express");
+const multer = require("multer");
+const Books = require("../models/books.model");
+const upload = multer({ dest: "uploads/" });
 
+const router = express.Router();
 
-const express = require('express')
-const multer = require('multer')
-const Books = require('../models/books.model')
-const upload = multer({ dest: 'uploads/' })
-
-const router = express.Router()
-
-
-router.get('/', (req,res) => {
-    res.send('Admin Dashboard !!')
-})
-
-// route to add new books
-router.post('/books',upload.single('file'), async (req,res) => {
-
-    try {
-
-        let book = await Books.findOne({title: req.body.title})
-
-        if (book){
-            return res.status(401).json('Book Already Added !!')
-        }
-
-
-        book = new Books({
-            title: req.body.title,
-            author:req.body.author,
-            publicationDate: req.body.publicationDate,
-            bookFile: req.file.path,
-            tags:req.body.tags
-        })
-
-        await book.save()
-
-        res.json(book)
-
-        
-    } catch (error) {
-
-        console.log(error)
-
-        return res.status(500).json({ error: error.message });    }
-})
-
-
-router.delete('/books/:id', async (req,res) => {
-
-    try {
-
-        let book = await Books.findByIdAndDelete(req.params.id)
-
-        res.json(book)
-
-        
-    } catch (error) {
-        console.log(error.message)
-        return res.status(500).json(error.message)
-    }
-
-})
-
-
-router.put('/books/:id', async (req, res) => {
-    try {
-        const book = await Books.findByIdAndUpdate(
-            req.params.id,
-            {
-                title: req.body.title,
-                author: req.body.author,
-                publicationDate: req.body.publicationDate,
-                bookFile: req.file ? req.file.path : undefined, 
-                tags: req.body.tags
-            },
-            { new: true } 
-        );
-
-        if (!book) {
-            return res.status(404).json({ message: 'Book not found' });
-        }
-
-        res.json(book);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: error.message });
-    }
+router.get("/", (req, res) => {
+  res.send("Admin Dashboard !!");
 });
 
+// route to add new books
+router.post("/books", upload.single("file"), async (req, res) => {
+  try {
+    let book = await Books.findOne({ title: req.body.title });
+
+    if (book) {
+      return res.status(401).json("Book Already Added !!");
+    }
+
+    book = new Books({
+      title: req.body.title,
+      author: req.body.author,
+      publicationDate: req.body.publicationDate,
+      bookFile: req.file.path,
+      tags: req.body.tags,
+    });
+
+    await book.save();
+
+    res.json(book);
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+router.delete("/books/:id", async (req, res) => {
+  try {
+    let book = await Books.findByIdAndDelete(req.params.id);
+
+    res.json(book);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json(error.message);
+  }
+});
+
+router.put("/books/:id", async (req, res) => {
+  try {
+    const book = await Books.findByIdAndUpdate(
+      {
+        title: req.body.title,
+        author: req.body.author,
+        publicationDate: req.body.publicationDate,
+        bookFile: req.file ? req.file.path : undefined,
+        tags: req.body.tags,
+      },
+      { new: true }
+    );
+
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.json(book);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router;
-
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+const connectDB = require("./models/connection");
+const Admin = require("./routes/admin");
+const Users = require("./routes/users");
+const authRouter = require("./routes/authRoute");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 4000
-const cors = require('cors')
-const path = require('path')
-const bodyParser = require('body-parser')
-const connectDB = require('./models/connection')
-const Admin = require('./routes/admin')
-const Users = require('./routes/users')
-const authRouter = require('./routes/authRoute')
+app.use("/admin", Admin);
+app.use("/users", Users);
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/auth", authRouter);
 
-app.use('/admin',Admin)
-app.use('/users', Users)
-
-app.use('/auth',authRouter)
-
-connectDB()
+connectDB();
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
-})
+  console.log(`server running on port ${PORT}`);
+});
